3374131a6c76f0f69b0c301422477595e04d955d
Rafael Prestes <rafap.ofcs2@gmail.com>
2022-07-28 17:05:53 -0300

    rolagem dos filmes

--- src/components/movieRow/MovieRow.jsx
+++ src/components/movieRow/MovieRow.jsx
@@ -4,14 +4,23 @@ import './movieRow.css'
 import { MdNavigateBefore, MdNavigateNext} from 'react-icons/md'
 
 const MovieRow = ({title, items}) => {
-    const [scrollX, setScrollX] = useState(-400)
+    const [scrollX, setScrollX] = useState(0)
 
     const handleLeftArrow = () => {
-        
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+        setScrollX(x)
     }
 
     const handleRightArrow = () => {
-        
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 201;
+        if((window.innerWidth - listW) > x ) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x)
     }
 
   return (
